39438b3593c54f51691af8599f8a77b6aee85663
Brian Lee <brianjleeofcl@gmail.com>
2017-03-16 11:11:17 -0700

    Add migration for users and devices

--- migrations/20170315185808_users.js
+++ migrations/20170315185808_users.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments()
+    table.string('name').notNullable()
+    table.string('email').unique().notNullable()
+    table.specificType('pw_hash', 'char(60)').notNullable()
+    table.timestamps(true, true)
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
 };
--- migrations/20170315190255_devices.js
+++ migrations/20170315190255_devices.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('devices', table => {
+    table.increments()
+    table.string('nickname').notNullable()
+    table.specificType('MAC_address', 'char(17)')
+    table.string('socket_id')
+    table.timestamps(true, true)
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('devices')
 };
