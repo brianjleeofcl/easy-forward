dc4148165304487efd5b853e98f8a21afb362bf8
Brian Lee <brianjleeofcl@gmail.com>
2017-03-21 18:36:03 -0700

    close #7 Add components for device control

--- device/mock-device-socket.js
+++ device/mock-device-socket.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client')
 const socket = io.connect('http://localhost:3000')
 
-const MAC = '02:A3:70:35:BC:A6'
+const MAC = '02:a3:70:35:bc:a6'
 
 socket.on('connect', () => {
   socket.emit('initialize-device-user', MAC)
@@ -12,10 +12,11 @@ socket.on('instructions', console.log)
 socket.on('record', ([interval, iteration]) => {
   let index = 0
   let intervalId = setInterval(() => {
-    console.log(Date.now())
+    console.log(Date.now(), '-', index)
     index++
     if (index === iteration) {
       clearInterval(intervalId)
+      socket.emit('recording-complete')
     }
   }, interval)
 })
--- src/app/main/devices/device-details/device-details.component.ts
+++ src/app/main/devices/device-details/device-details.component.ts
@@ -9,13 +9,35 @@ import { SocketService } from '../../../socket.service';
 
 import { Device } from '../../../device';
 
+enum TimeUnit {Second = 1000, Minute = 60000, Hour = 3600000}
+
+class InstructionForm {
+  duration: number;
+  duration_unit_val: TimeUnit;
+  get duration_unit(): string {
+    return TimeUnit[this.duration_unit_val]
+  }
+  set duration_unit(unit: string) {
+    this.duration_unit_val = TimeUnit[unit]
+  }
+  interval: number;
+  interval_unit_val: TimeUnit;
+  get interval_unit(): string {
+    return TimeUnit[this.interval_unit_val]
+  }
+  set interval_unit(unit: string) {
+    this.interval_unit_val = TimeUnit[unit]
+  }
+}
+
 @Component({
   selector: 'app-device-details',
   templateUrl: './device-details.component.html',
   styleUrls: ['./device-details.component.css']
 })
 export class DeviceDetailsComponent implements OnInit {
-  device: Device
+  device: Device;
+  model: InstructionForm;
 
   constructor(
     private socket: SocketService,
@@ -24,14 +46,28 @@ export class DeviceDetailsComponent implements OnInit {
     private location: Location
   ) { 
     this.device = new Device()
+    this.model = new InstructionForm()
   }
 
   ngOnInit() {
     this.route.params.switchMap((params: Params) => this.dS.getDevice(+params['id']))
       .subscribe(device => this.device = device);
+    this.model.duration_unit_val = TimeUnit.Minute;
+    this.model.interval_unit_val = TimeUnit.Second;
+  }
+
+  setUnit(target, unit) {
+    this.model[target] = unit
   }
 
   start(): void {
-    this.socket.sendInstructions(this.device.socket_id, 1000, 10)
+    const {duration, duration_unit_val, interval, interval_unit_val} = this.model
+    const iteration: number = (duration * duration_unit_val) / (interval * interval_unit_val);
+    
+    this.socket.sendInstructions(this.device.socket_id, interval * interval_unit_val, iteration)
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.model)
   }
 }
