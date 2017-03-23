65aab0d2badfcd5726a29fb2e440ad3e22f2c074
Brian Lee <brianjleeofcl@gmail.com>
2017-03-23 01:03:26 -0700

    Close #8 Add server routes for projects

--- migrations/20170322232840_projects.js
+++ migrations/20170322232840_projects.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
     table.increments()
     table.integer('user_id').references('id').inTable('users').notNullable().index()
     table.string('hash_id')
-    table.integer('last_frame_index')
+    table.integer('last_frame_index').defaultTo(0)
     table.string('published_url').defaultTo(null)
     table.timestamps(true, true)
   })
---
+++ server/api/routes/projects.js
@@ -0,0 +1,49 @@
+const knex = require('../../knex');
+
+const express = require('express');
+const router = express.Router();
+
+const HashId = require('hashids');
+const hash = new HashId('', 10)
+const jwt = require('jsonwebtoken');
+const boom = require('boom');
+
+function auth(req, res, next) {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, claim) => {
+    if (err) {
+      next(boom.unauthorized('Invalid access token'));
+    }
+
+    req.claim = claim
+    next()
+  })
+};
+
+router.get('/', auth, (req, res, next) => {
+  knex('projects').where('user_id', req.claim.id).then((projects) => res.send(projects))
+});
+
+router.get('/:id', auth, (req, res, next) => {
+  knex('projects').where('id', req.params.id).then(([project]) => {
+    if (!project) {
+      next(boom.notFound('No project matching the id was found'))
+    }
+    if (project.user_id !== req.claim.id) {
+      next(boom.unauthorized('Invalid access token'))
+    }
+
+    res.send(project)
+  })
+})
+
+router.post('/new', auth, (req, res, next) => {
+  const user_id = req.claim.id
+
+  knex('projects').insert({user_id}, '*').then(([project]) => {
+    const hash_id = hash.encode(project.id)
+
+    return knex('projects').where('id', project.id).update({hash_id}, '*')
+  }).then(([augProject]) => res.send(augProject))
+});
+
+module.exports = router;
