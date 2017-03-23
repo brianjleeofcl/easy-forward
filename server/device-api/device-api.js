9c7ac488a8860d302c6031bfac34490a66df2af3
Brian Lee <brianjleeofcl@gmail.com>
2017-03-23 01:31:51 -0700

    Close #6 Configure AWS S3

--- server/device-api/device-api.js
+++ server/device-api/device-api.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
-const boom = require('boom')
+const boom = require('boom');
 
-router.use('/streams', require('./routes/streams'))
+router.use('/streams', require('./routes/streams'));
+router.use('/post-image', require('./routes/post-image'))
 
 router.use((req, res, next) => {
   next(boom.notFound())
--- server/device-api/routes/post-image.js
+++ server/device-api/routes/post-image.js
@@ -3,26 +3,43 @@ const knex = require('../../knex');
 const express = require('express');
 const router = express.Router();
 
-const jwt = require('jsonwebtoken');
+const aws = require('aws-sdk')
+const s3 = new aws.S3()
 
+const leftPad = require('left-pad');
 const boom = require('boom');
 
-function auth(req, res, next) {
-  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, claim) => {
-    if (err) {
-      next(boom.unauthorized('Invalid access token'));
-    }
+router.post('/:hash/:index', (req, res, next) => {
+  const { hash, index } = req.params;
+  const paddedIndex = leftPad(index, 3, '0');
+  const reqBuffer = [];
 
-    req.claim = claim
-    next()
+  req.on('data', chunk => reqBuffer.push(chunk)).on('end', () => {
+    const reqBody = Buffer.concat(reqBuffer)
+    s3.putObject({
+      ACL: 'public-read',
+      Bucket: 'brianjleeofcl-capstone',
+      Key: `${hash}-${paddedIndex}.jpg`,
+      Body: reqBody,
+      ContentEncoding: 'base64',
+      ContentType: 'image/jpg',
+    }, (err, data) => {
+      if (err) {
+        console.error(err);
+        next(boom.notImplemented('S3 Error', err))
+      } else {
+        knex('projects').where('hash_id', hash).then(([project]) => {
+          const last_frame_index = project.last_frame_index < index 
+            ? index 
+            : project.last_frame_index;
+          
+          return knex('projects').where('hash_id', hash).update({last_frame_index}, '*')
+        }).then(([project]) => {
+          res.send(project.last_frame_index)
+        })
+      }
+    })
   })
-};
-
-// router.get('/:id', auth, (req, res, next) => {
-//   // knex('devices').where('id', req.params.id).then(([device]) => {
-//   //   device.socket_id
-//   // })
-//   next(boom.notFound())
-// })
+})
 
 module.exports = router;
