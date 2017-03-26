4dafe9751d1825cb87e5b568b5e6e37ac1218988
Brian Lee <brianjleeofcl@gmail.com>
2017-03-25 21:47:55 -0700

    Close #21 Add gallery service

--- src/app/gallery.service.ts
+++ src/app/gallery.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class GalleryService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  getImages() {
+    return this.http.get('/api/images').toPromise().then(res => res.json()).catch(this.handleError)
+  }
+
+  getImage(url: string) {
+    return this.http.get(`/api/images/${url}`).toPromise().then(res => res.json()).catch(this.handleError)
+  }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error)
+  }
 }
